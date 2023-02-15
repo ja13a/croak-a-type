c3a6da506ec8004d115be01603b15731c4dff146
ja13a <suvorovsergey99@gmail.com>
2023-02-15 19:06:42 +0000

    fix: .ts files linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,52 +1,41 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  env: {
+    browser: true,
+    es2021: true
   },
-  'extends': [
+  extends: [
     'eslint:recommended',
-    'prettier'
+    'prettier',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
-  'rules': {
-    'indent': [
-      'warn',
-      2
-    ],
-    'linebreak-style': [
-      'warn',
-      'unix'
-    ],
-    'quotes': [
-      'warn',
-      'single'
-    ],
-    'semi': [
-      'warn',
-      'always'
-    ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    indent: ['warn', 2],
+    'linebreak-style': ['warn', 'unix'],
+    quotes: ['warn', 'single'],
+    semi: ['warn', 'always'],
     'no-trailing-spaces': [
       'error',
       {
-        'skipBlankLines': false
+        skipBlankLines: false
       }
     ],
     'max-len': [
       'error',
       {
-        'code': 120
+        code: 120
       }
     ],
-    'comma-dangle': [
-      'error',
-      'never'
-    ],
+    'comma-dangle': ['error', 'never']
   },
-  ignorePatterns: [
-    '/config/**',
-    '.eslintrc.js'
-  ]
+  ignorePatterns: ['/config/**', '.eslintrc.js']
 };
